dd326851611e99b936fc5d08937cea70db9bb833
Xnuk Shuman <admin@xnu.kr>
2023-03-29 02:10:29 +0900

    fish update

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,4 @@
 function fish_greeting
-	df -Ph ~ | sed -n 2p | read -l _ all used avail percent _
+	df -Ph ~ | sed -n 2p | read -l dummy all used avail percent dummy
 	echo "Disk: $percent used ($used / $all), $avail available."
 end
-
