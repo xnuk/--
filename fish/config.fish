c7cb4f7b4a269545e98b2ba3ddbf487aec2a3943
Xnuk Shuman <admin@xnu.kr>
2024-07-05 09:14:44 +0900

    Revert "lix config"
    This reverts commit 0e4d5389fc11b9e6b26456f0cf7b6fa5d278e9d1.

--- fish/config.fish
+++ fish/config.fish
@@ -127,8 +127,3 @@ set -Ux fish_color_error ff0000
 set -Ux fish_color_param 00afff
 set -Ux fish_color_quote 999900
 set -Ux fish_color_redirection 00afff
-
-# nix
-if test -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-	source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-end
